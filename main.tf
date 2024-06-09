3f802fde7a06b50d119c5e2cde3c71301b74f7a3
Shubham Kumar <sraj94710@gmail.com>
2024-06-10 01:33:40 +0530

    changes in terraform file

--- main.tf
+++ main.tf
@@ -1,82 +1,23 @@
 provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_ecs_cluster" "hello-world-cluster" {
-  name = "hello-world-cluster"
-}
-
-resource "aws_ecs_task_definition" "hello-world-task" {
-  family                   = "hello-world-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-
-  container_definitions = jsonencode([{
-    name      = "hello-world-container"
-    image     = "${aws_ecr_repository.hello_world_repo.repository_url}:latest"
-    cpu       = 256
-    memory    = 512
-    essential = true
-    portMappings = [{
-      containerPort = 3000
-      hostPort      = 3000
-    }]
-  }])
-}
-
-resource "aws_ecs_service" "hello-world-service" {
-  name            = "hello-world-service"
-  cluster         = aws_ecs_cluster.hello-world-cluster.id
-  task_definition = aws_ecs_task_definition.hello-world-task.arn
-  desired_count   = 1
-
-  network_configuration {
-    subnets         = [aws_subnet.public.id]
-    security_groups = [aws_security_group.ecs_sg.id]
-  }
-}
-
-resource "aws_ecr_repository" "hello_world_repo" {
-  name = "hello-world-repo"
-}
-
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
-
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  ]
+  region = "us-west-2"
 }
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
 }
 
-resource "aws_security_group" "ecs_sg" {
+resource "aws_security_group" "ecs" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -86,5 +27,41 @@ resource "aws_security_group" "ecs_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "hello-world-cluster"
+}
+
+resource "aws_ecs_task_definition" "task" {
+  family                = "hello-world-task"
+  network_mode          = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                   = "256"
+  memory                = "512"
+
+  container_definitions = jsonencode([{
+    name  = "hello-world"
+    image = var.docker_image
+    cpu   = 256
+    memory = 512
+    essential = true
+    portMappings = [{
+      containerPort = 3000
+      hostPort      = 3000
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "service" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = [aws_subnet.subnet.id]
+    security_groups = [aws_security_group.ecs.id]
   }
 }
